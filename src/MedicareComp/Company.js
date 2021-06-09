6c5ffff4f755e893d248ff0afe3a9711b1f9f6a0
Kriti Agarwal <kritiagarwal2098@gmail.com>
2021-06-09 22:38:45 +0530

    Adding companies name
    Signed-off-by: Kriti Agarwal <kritiagarwal2098@gmail.com>

---
+++ src/MedicareComp/Company.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+export class Company extends Component {
+    render() {
+        return (
+            <div>
+                <div className="com-cont">
+                    <div className="com">
+
+                    </div>
+                    <div className="com">
+
+                    </div>
+                    <div className="com">
+
+                    </div>
+                    <div className="com">
+
+                    </div>
+                    <div className="com">
+
+                    </div>
+                </div>
+                <div className="com-cont com-cont2">
+                    <div className="com">
+
+                    </div>
+                    <div className="com">
+
+                    </div>
+                    <div className="com">
+
+                    </div>
+                    <div className="com">
+
+                    </div>
+                </div>
+                <div className="hexa-cont">
+                    <div class="hexagon"><span></span></div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Company
