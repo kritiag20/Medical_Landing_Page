ca21c835c248feb77237a732ecfe865fd379be08
Kriti Agarwal <kritiagarwal2098@gmail.com>
2021-06-11 23:28:30 +0530

    Created Hexagon using HTML, SASS
    Signed-off-by: Kriti Agarwal <kritiagarwal2098@gmail.com>

--- src/MedicareComp/Company.js
+++ src/MedicareComp/Company.js
@@ -5,39 +5,69 @@ export class Company extends Component {
         return (
             <div>
                 <div className="com-cont">
-                    <div className="com">
-
+                    <div className="com-main">
+                        <div className="com"></div>
                     </div>
-                    <div className="com">
-
+                    <div className="com-main">
+                        <div className="com"></div>
                     </div>
-                    <div className="com">
-
+                    <div className="com-main">
+                        <div className="com"></div>
                     </div>
-                    <div className="com">
-
+                    <div className="com-main">
+                        <div className="com"></div>
                     </div>
-                    <div className="com">
-
-                    </div>
-                </div>
-                <div className="com-cont com-cont2">
-                    <div className="com">
-
-                    </div>
-                    <div className="com">
-
-                    </div>
-                    <div className="com">
-
-                    </div>
-                    <div className="com">
-
+                    <div className="com-main">
+                        <div className="com"></div>
                     </div>
                 </div>
+
+                {/* <div className="com-cont com-cont2">
+                    <div className="com">
+                    </div>
+                    <div className="com">
+                    </div>
+                    <div className="com">
+                    </div>
+                    <div className="com">
+                    </div>
+                </div> */}
                 <div className="hexa-cont">
-                    <div class="hexagon"><span></span></div>
+                    <div className="hexagon">
+                        <div class="hexa"><span></span></div>
+                    </div>
+                    <div className="hexagon">
+                        <div class="hexa"><span></span></div>
+                    </div>
+                    <div className="hexagon">
+                        <div class="hexa"><span></span></div>
+                    </div>
+                    <div className="hexagon">
+                        <div class="hexa"><span></span></div>
+                    </div>
+                    <div className="hexagon">
+                        <div class="hexa"><span></span></div>
+                    </div>
                 </div>
+                <div className="hexa-cont hexagon1">
+                    <div className="hexagon">
+                        <div class="hexa"><span></span></div>
+                    </div>
+                    <div className="hexagon">
+                        <div class="hexa"><span></span></div>
+                    </div>
+                    <div className="hexagon">
+                        <div class="hexa"><span></span></div>
+                    </div>
+                    <div className="hexagon">
+                        <div class="hexa"><span></span></div>
+                    </div>
+                </div>
+                {/* <div className="hexagon">
+                    <div class="hexa"><span>
+                        <div class="hexa"><span></span></div>
+                    </span></div>
+                </div> */}
             </div>
         )
     }
