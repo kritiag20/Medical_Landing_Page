a1f9f1500a0f72433c78d45d9dee3df1a1dc93c0
Kriti Agarwal <kritiagarwal2098@gmail.com>
2021-06-12 23:51:20 +0530

    Rotating square
    Signed-off-by: Kriti Agarwal <kritiagarwal2098@gmail.com>

---
+++ src/MedicareComp/Rotator.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Rotator extends Component {
+    render() {
+        return (
+            <div>
+                <div class="origin"></div>
+            </div>
+        )
+    }
+}
+
+export default Rotator
