cea6c5d5dc93aa62be014015bb42552dad9320ce
Kriti Agarwal <kritiagarwal2098@gmail.com>
2021-06-20 23:30:09 +0530

    Google Icon Using CSS
    Signed-off-by: Kriti Agarwal <kritiagarwal2098@gmail.com>

---
+++ src/MedicareComp/GoogleIcon.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class GoogleIcon extends Component {
+    render() {
+        return (
+            <div>
+                <div className="logo"></div>
+            </div>
+        )
+    }
+}
+
+export default GoogleIcon
